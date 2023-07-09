0ed2766d13ca41dd5f790e89315e0a84a4b69236
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-07-09 22:59:29 +0530

    fixed-some-error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 
 const http = require('http').createServer(app)
 
-const PORT = process.env.PORT || 3030
+const PORT = process.env.PORT || 3000
 
 
 http.listen(PORT,()=>{
@@ -23,5 +23,8 @@ const io = require('socket.io')(http)
 
 io.on('connection',(socket)=>{
     console.log('Connected');
+    socket.on('message',(msg)=>{
+        socket.broadcast.emit('message',msg)
+    })
 })
 
